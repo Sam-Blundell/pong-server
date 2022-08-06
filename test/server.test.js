e1c18f16193998d3d018e3335b71b5792e931cc3
Sam Blundell <samcalblundell@googlemail.com>
2022-08-06 02:30:47 +0100

    set up basic websocket functions and refactor project to use reusable http server instance

--- test/server.test.js
+++ test/server.test.js
@@ -1,24 +1,24 @@
 import { expect } from 'chai';
 import request from 'supertest';
-import server from '../server.js';
+import { app } from '../server.js';
 
-describe('Server Testing', () => {
+describe('app Testing', () => {
     describe('hello world', () => {
-        it('GET:200, should respond with "hello world".', () => request(server).get('/')
+        it('GET:200, should respond with "hello world".', () => request(app).get('/')
             .expect(200)
             .then((response) => {
                 expect(response.body.msg).to.equal('hello world');
             }));
     });
     describe('forbidden methods', () => {
-        it('POST/PATCH/ETC:405, should return 405 method not allowed when / is given anything other than a get request', () => request(server).post('/')
+        it('POST/PATCH/ETC:405, should return 405 method not allowed when / is given anything other than a get request', () => request(app).post('/')
             .expect(405)
             .then((response) => {
                 expect(response.body.msg).to.equal('405 Method Not Allowed');
             }));
     });
     describe('404 Error', () => {
-        it('GET:404, should respond with appropriate error and error message', () => request(server).get('/doesntexist')
+        it('GET:404, should respond with appropriate error and error message', () => request(app).get('/doesntexist')
             .expect(404)
             .then((response) => {
                 expect(response.body.msg).to.equal('404 Page Not Found');
