047d563203c34d439df94b6869bd072efb3c2e53
Sam Blundell <samcalblundell@googlemail.com>
2022-08-08 17:04:37 +0100

    extract setup procedure for a new game into it's own file

---
+++ gameSetup.js
@@ -0,0 +1,36 @@
+const validKeys = {
+    playerOne: {
+        ArrowLeft: false,
+        ArrowRight: false,
+    },
+    playerTwo: {
+        ArrowLeft: false,
+        ArrowRight: false,
+    },
+};
+
+const initialGameState = {
+    playerOnePos: 220,
+    playerTwoPos: 220,
+    ballXPos: 246,
+    ballYPos: 346,
+    ballHSpeed: 1,
+    ballVSpeed: 3,
+    score: [0, 0],
+};
+
+const initialClients = {
+    playerOne: null,
+    playerTwo: null,
+};
+
+const gameSetup = () => {
+    const game = {
+        clients: { ...initialClients },
+        keys: { ...validKeys },
+        gameState: { ...initialGameState },
+    };
+    return game;
+};
+
+export default gameSetup;
