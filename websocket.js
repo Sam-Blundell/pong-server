f11f4ade763fa32e8fe0e28b7cc9977e88d31b76
Sam Blundell <samcalblundell@googlemail.com>
2022-08-09 01:25:51 +0100

    refactor websocket file now that some functions and templates have been extracted, generally clear up the logic a little

--- websocket.js
+++ websocket.js
@@ -1,56 +1,79 @@
 import { WebSocketServer } from 'ws';
+import { nanoid } from 'nanoid/non-secure';
 import gameSetup from './gameSetup.js';
+import playerUpdate from './updateMethods.js';
 
 const setupSocketServer = (server) => {
     const wss = new WebSocketServer({ server, clientTracking: true });
 
-    const game = gameSetup();
+    const games = {};
 
-    const playerUpdate = (userType) => {
-        const { keys, gameState } = game;
-        if (keys[userType].ArrowRight === true && gameState[`${userType}Pos`] < 440) {
-            gameState[`${userType}Pos`] += 5;
-        } else if (keys[userType].ArrowLeft === true && gameState[`${userType}Pos`] > 0) {
-            gameState[`${userType}Pos`] -= 5;
-        }
-    };
+    const playerOneSetup = (gameID) => {
+        const { playerOne: socket } = games[gameID].clients;
+        const { playerOne: keys } = games[gameID].keys;
+        const { gameState } = games[gameID];
 
-    const socketHandler = (socket) => {
-        let userType = null;
-        const { keys, clients, gameState } = game;
         socket.on('message', (data) => {
             const parsedData = JSON.parse(data);
-            if ('userType' in parsedData) {
-                userType = parsedData.userType;
-                clients[parsedData.userType] = socket;
-                if (userType === 'playerTwo') {
-                    const msg = JSON.stringify({ opponentConnected: true });
-                    socket.send(msg);
-                    clients.playerOne.send(msg);
-                }
-            } else if ('updateState' in parsedData) {
-                playerUpdate(userType);
+            if ('inputData' in parsedData) {
+                const [input, action] = parsedData.inputData;
+                keys[input] = action;
+            }
+            if ('updateState' in parsedData) {
+                playerUpdate(keys, games[gameID], 'playerOnePos');
                 const msg = JSON.stringify({ gameState });
                 socket.send(msg);
-            } else if ('inputData' in parsedData) {
-                const [, input, action] = parsedData.inputData;
-                keys[userType][input] = action;
             }
         });
-        socket.on('close', () => {
-            clients[userType] = null;
-            const msg = JSON.stringify({ opponentDisconnected: true });
-            if (userType === 'playerOne' && clients.playerTwo !== null) {
-                clients.playerTwo.send(msg);
-            } else if (userType === 'playerTwo' && clients.playerOne !== null) {
-                clients.playerOne.send(msg);
+        socket.on('close', (data) => {
+            const parsedData = JSON.parse(data);
+            console.log(parsedData);
+        });
+    };
+    const playerTwoSetup = (gameID) => {
+        const { playerTwo: socket } = games[gameID].clients;
+        const { playerTwo: keys } = games[gameID].keys;
+        const { gameState } = games[gameID];
+
+        socket.on('message', (data) => {
+            const parsedData = JSON.parse(data);
+            if ('inputData' in parsedData) {
+                const [input, action] = parsedData.inputData;
+                keys[input] = action;
             }
+            if ('updateState' in parsedData) {
+                playerUpdate(keys, games[gameID], 'playerTwoPos');
+                const msg = JSON.stringify({ gameState });
+                socket.send(msg);
+            }
+        });
+        socket.on('close', (data) => {
+            const parsedData = JSON.parse(data);
+            console.log(parsedData);
         });
     };
 
-    wss.on('connection', (socket) => {
-        console.log('New client connected.');
-        socketHandler(socket);
+    wss.on('connection', (socket, request) => {
+        if (request.url === '/start') {
+            const gameID = nanoid(6);
+            games[gameID] = gameSetup(socket, gameID);
+            socket.send(JSON.stringify({ gameID }));
+            playerOneSetup(gameID);
+        } else if (request.url.startsWith('/join')) {
+            const gameID = request.url.slice('5');
+            if (gameID in games === false) {
+                socket.send(JSON.stringify({ ERR: 'NoSuchGame' }));
+                socket.close();
+            } else if (games[gameID].clients.playerTwo !== null) {
+                socket.send(JSON.stringify({ ERR: 'Game Full' }));
+                socket.close();
+            } else {
+                games[gameID].clients.playerTwo = socket;
+                games[gameID].ready = true;
+                playerTwoSetup(gameID);
+                games[gameID].clients.playerOne.send(JSON.stringify({ opponentConnected: true }));
+            }
+        }
     });
 };
 
