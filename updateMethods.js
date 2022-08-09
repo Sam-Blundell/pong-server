37bad1ac9b06f1b28ce0e1f208cc26030d6f8c1f
Sam Blundell <samcalblundell@googlemail.com>
2022-08-09 20:40:08 +0100

    refactor updateMethods.js to replace magic numbers with variables and rename functions to be more semantic

--- updateMethods.js
+++ updateMethods.js
@@ -3,11 +3,15 @@ const paddleLeftBoundary = 0;
 const paddleSpeed = 5;
 const ballRightBoundary = 493;
 const ballLeftBoundary = 0;
-// const ballMinVSpeed = 3;
-// const ballMaxVSpeed = 12;
-// const ballMaxHSpeed = 6;
+const ballMaxVSpeed = 10;
 const screenTop = 0;
 const screenBottom = 693;
+const playerOneBottom = 657;
+const playerOneTop = 650;
+const playerTwoBottom = 57;
+const playerTwoTop = 50;
+const ballWidth = 7;
+const paddleWidth = 60;
 
 const playerUpdate = (keys, game, playerPos) => {
     const { gameState } = game;
@@ -20,13 +24,15 @@ const playerUpdate = (keys, game, playerPos) => {
 
 const ballUpdate = (game) => {
     const { gameState } = game;
-    const paddleCheck = (playerPos, ballPos) => {
-        if (ballPos + 7 >= playerPos && ballPos <= playerPos + 60) {
-            const speedMod = Math.abs(gameState.ballVSpeed) > 8 ? -1 : -1.1;
+
+    const paddleCollisionCheck = (playerPos, ballPos) => {
+        if (ballPos + ballWidth >= playerPos && ballPos <= playerPos + paddleWidth) {
+            const speedMod = Math.abs(gameState.ballVSpeed) > ballMaxVSpeed ? -1 : -1.05;
             gameState.ballVSpeed *= speedMod;
         }
     };
-    const ballServe = () => {
+
+    const resetPositions = () => {
         gameState.playerOnePos = 220;
         gameState.playerTwoPos = 220;
         gameState.ballHSpeed = 1;
@@ -34,24 +40,25 @@ const ballUpdate = (game) => {
         gameState.ballXPos = 246;
         gameState.ballYPos = 346;
     };
+
     gameState.ballYPos += gameState.ballVSpeed;
     gameState.ballXPos += gameState.ballHSpeed;
     if (gameState.ballYPos <= screenTop) {
         gameState.score[0] += 1;
-        ballServe();
+        resetPositions();
     }
     if (gameState.ballYPos >= screenBottom) {
         gameState.score[1] += 1;
-        ballServe();
+        resetPositions();
     }
     if (gameState.ballXPos >= ballRightBoundary || gameState.ballXPos <= ballLeftBoundary) {
         gameState.ballHSpeed *= -1;
     }
-    if ((gameState.ballYPos + 7 > 650) && (gameState.ballYPos < 657)) {
-        paddleCheck(gameState.playerOnePos, gameState.ballXPos);
+    if ((gameState.ballYPos + ballWidth > playerOneTop) && (gameState.ballYPos < playerOneBottom)) {
+        paddleCollisionCheck(gameState.playerOnePos, gameState.ballXPos);
     }
-    if ((gameState.ballYPos + 7 > 50) && (gameState.ballYPos < 57)) {
-        paddleCheck(gameState.playerTwoPos, gameState.ballXPos);
+    if ((gameState.ballYPos + ballWidth > playerTwoTop) && (gameState.ballYPos < playerTwoBottom)) {
+        paddleCollisionCheck(gameState.playerTwoPos, gameState.ballXPos);
     }
 };
 
