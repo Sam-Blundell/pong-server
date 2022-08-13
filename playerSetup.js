1446ceb283b99cdbf8df58c4b0eee15e8d76dfb1
Sam Blundell <samcalblundell@googlemail.com>
2022-08-13 03:00:10 +0100

    remove socket listener for gamestate update requests, extract gamestate updating logic to main file

--- playerSetup.js
+++ playerSetup.js
@@ -1,8 +1,5 @@
-import { playerUpdate, ballUpdate } from './updateMethods.js';
-
 const playerSetup = (game, player) => {
     const { [player]: socket } = game.clients;
-    const { gameState, keys } = game;
 
     socket.binaryType = 'arraybuffer';
 
@@ -10,16 +7,8 @@ const playerSetup = (game, player) => {
         if (data instanceof ArrayBuffer) {
             // 3 bytes. 1 identifies inputData, 2 specifies key, 3 specifies condition.
             const inputView = new DataView(data);
-            const keysView = new DataView(keys[player]);
+            const keysView = new DataView(game.keys[player]);
             keysView.setUint8(inputView.getUint8(1), inputView.getUint8(2));
-        } else {
-            const parsedData = JSON.parse(data);
-            if ('updateState' in parsedData) {
-                playerUpdate(keys[player], game, `${player}Pos`);
-                ballUpdate(game);
-                const msg = JSON.stringify({ gameState });
-                socket.send(msg);
-            }
         }
     });
 };
