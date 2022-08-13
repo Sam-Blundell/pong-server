3b73219891eb843fd8fcc3e9982ee038ec8b69f4
Sam Blundell <samcalblundell@googlemail.com>
2022-08-13 22:24:30 +0100

    replace gameState object with an arraybuffer and instantiate it

--- gameSetup.js
+++ gameSetup.js
@@ -4,15 +4,17 @@ const validKeys = {
     playerTwo: new ArrayBuffer(2),
 };
 
-const initialGameState = {
-    playerOnePos: 220,
-    playerTwoPos: 220,
-    ballXPos: 246,
-    ballYPos: 346,
-    ballHSpeed: 1,
-    ballVSpeed: 3,
-    score: [0, 0],
-};
+const gameStateBuffer = new ArrayBuffer(19);
+const stateView = new DataView(gameStateBuffer);
+stateView.setUint8(0, 20); // identifier
+stateView.setUint16(1, 220); // playerOnePos
+stateView.setUint16(3, 220); // playerTwoPos
+stateView.setUint16(5, 246); // ballXPos
+stateView.setUint16(7, 346); // ballYPos
+stateView.setFloat32(9, 1); // ballHSpeed
+stateView.setFloat32(13, 3); // ballVSpeed
+stateView.setUint8(17, 0); // playerOneScore
+stateView.setUint8(18, 0); // playerTwoScore
 
 const initialClients = {
     playerOne: null,
@@ -23,7 +25,7 @@ const gameSetup = (player) => {
     const game = {
         clients: { ...initialClients },
         keys: { ...validKeys },
-        gameState: { ...initialGameState },
+        binaryGameState: gameStateBuffer,
         ready: false,
     };
     game.clients.playerOne = player;
