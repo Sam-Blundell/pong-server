8aeb442a1f3a53ed1b045ac63d677b57f50eb103
Sam Blundell <samcalblundell@googlemail.com>
2022-08-14 19:17:53 +0100

    update update methods to use new state getters/setters

--- updateMethods.js
+++ updateMethods.js
@@ -1,3 +1,22 @@
+import {
+    getPlayerOnePos,
+    getPlayerTwoPos,
+    getBallXPos,
+    getBallYPos,
+    getBallHSpeed,
+    getBallVSpeed,
+    getPlayerOneScore,
+    getPlayerTwoScore,
+    setPlayerOnePos,
+    setPlayerTwoPos,
+    setBallXPos,
+    setBallYPos,
+    setBallHSpeed,
+    setBallVSpeed,
+    setPlayerOneScore,
+    setPlayerTwoScore,
+} from './gameStateManager.js';
+
 const paddleRightBoundary = 440;
 const paddleLeftBoundary = 0;
 const paddleSpeed = 5;
@@ -5,7 +24,7 @@ const ballRightBoundary = 493;
 const ballLeftBoundary = 0;
 const ballMaxVSpeed = 10;
 const ballMaxHSpeed = 6;
-const screenTop = 0;
+const screenTop = 1;
 const screenBottom = 693;
 const playerOneBottom = 657;
 const playerOneTop = 650;
@@ -13,8 +32,6 @@ const playerTwoBottom = 57;
 const playerTwoTop = 50;
 const ballWidth = 7;
 const paddleWidth = 60;
-
-// this is a mess and needs either commenting or refactoring.
 
 const playerUpdate = (keys, game, playerIndex) => {
     const stateView = new DataView(game.binaryGameState);
@@ -30,54 +47,60 @@ const playerUpdate = (keys, game, playerIndex) => {
 };
 
 const ballUpdate = (game) => {
-    const stateView = new DataView(game.binaryGameState);
+    const { binaryGameState: buffer } = game;
 
     const paddleCollisionCheck = (playerPos, ballPos) => {
-        if (ballPos + ballWidth >= playerPos && ballPos <= playerPos + paddleWidth) {
-            const speedMod = Math.abs(stateView.getFloat32(13)) > ballMaxVSpeed ? -1 : -1.05;
+        if (
+            (ballPos + ballWidth) >= playerPos
+            && ballPos <= (playerPos + paddleWidth)
+        ) {
+            const speedMod = Math.abs(getBallVSpeed(buffer)) > ballMaxVSpeed ? -1 : -1.05;
             const offSet = ((ballPos + (ballWidth / 2) - (playerPos + (paddleWidth / 2))) / 15);
 
-            stateView.setFloat32(
-                9,
-                Math.min(Math.max(stateView.getFloat32(9) + offSet, -ballMaxHSpeed), ballMaxHSpeed),
+            setBallHSpeed(
+                buffer,
+                Math.min(
+                    Math.max(getBallHSpeed(buffer) + offSet, -ballMaxHSpeed),
+                    ballMaxHSpeed,
+                ),
             );
-            stateView.setFloat32(13, stateView.getFloat32(13) * speedMod);
+            setBallVSpeed(buffer, getBallVSpeed(buffer) * speedMod);
         }
     };
 
     const resetPositions = () => {
-        stateView.setUint16(1, 220);
-        stateView.setUint16(3, 220);
-        stateView.setUint16(5, 246);
-        stateView.setUint16(7, 346);
-        stateView.setFloat32(9, 1);
-        stateView.setFloat32(13, 3);
+        setPlayerOnePos(buffer, 220);
+        setPlayerTwoPos(buffer, 220);
+        setBallXPos(buffer, 246);
+        setBallYPos(buffer, 346);
+        setBallHSpeed(buffer, 1);
+        setBallVSpeed(buffer, 3);
     };
 
-    stateView.setUint16(5, stateView.getUint16(5) + Math.round(stateView.getFloat32(9)));
-    stateView.setUint16(7, stateView.getUint16(7) + Math.round(stateView.getFloat32(13)));
+    setBallXPos(buffer, getBallXPos(buffer) + Math.round(getBallHSpeed(buffer)));
+    setBallYPos(buffer, getBallYPos(buffer) + Math.round(getBallVSpeed(buffer)));
 
-    if (stateView.getUint16(7) <= screenTop) {
-        stateView.setUint8(17, stateView.getUint8(17) + 1);
+    if (getBallYPos(buffer) <= screenTop) {
+        setPlayerOneScore(buffer, getPlayerOneScore(buffer) + 1);
         resetPositions();
-    } else if (stateView.getUint16(7) >= screenBottom) {
-        stateView.setUint8(18, stateView.getUint8(18) + 1);
+    } else if (getBallYPos(buffer) >= screenBottom) {
+        setPlayerTwoScore(buffer, getPlayerTwoScore(buffer) + 1);
         resetPositions();
     } else if (
-        stateView.getUint16(5) >= ballRightBoundary
-        || stateView.getUint16(5) <= ballLeftBoundary
+        getBallXPos(buffer) >= ballRightBoundary
+        || getBallXPos(buffer) <= ballLeftBoundary
     ) {
-        stateView.setFloat32(9, stateView.getFloat32(9) * -1);
+        setBallHSpeed(buffer, getBallHSpeed(buffer) * -1);
     } else if (
-        (stateView.getUint16(7) + ballWidth > playerOneTop)
-        && (stateView.getUint16(7) < playerOneBottom)
+        (getBallYPos(buffer) + ballWidth) > playerOneTop
+        && getBallYPos(buffer) < playerOneBottom
     ) {
-        paddleCollisionCheck(stateView.getUint16(1), stateView.getUint16(5));
+        paddleCollisionCheck(getPlayerOnePos(buffer), getBallXPos(buffer));
     } else if (
-        (stateView.getUint16(7) + ballWidth > playerTwoTop)
-        && (stateView.getUint16(7) < playerTwoBottom)
+        (getBallYPos(buffer) + ballWidth) > playerTwoTop
+        && getBallYPos(buffer) < playerTwoBottom
     ) {
-        paddleCollisionCheck(stateView.getUint16(3), stateView.getUint16(5));
+        paddleCollisionCheck(getPlayerTwoPos(buffer), getBallXPos(buffer));
     }
 };
 
