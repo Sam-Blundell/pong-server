f1cc19f33ef5ddfda2fe68b0f3656345da991988
Sam Blundell <samcalblundell@googlemail.com>
2022-08-14 19:17:24 +0100

    create functions for creating a gamestate buffer and getting/setting values

--- gameStateManager.js
+++ gameStateManager.js
@@ -1,4 +1,4 @@
-const byteLength = 19;
+const totalBytes = 19;
 const bufferVal = {
     identifier: 0,
     playerOnePos: 1,
@@ -12,7 +12,7 @@ const bufferVal = {
 };
 
 const createGameStateBuffer = () => {
-    const buffer = new ArrayBuffer(byteLength);
+    const buffer = new ArrayBuffer(totalBytes);
     const view = new DataView(buffer);
     view.setUint8(bufferVal.identifier, 20);
     view.setUint16(bufferVal.playerOnePos, 220);
@@ -27,4 +27,102 @@ const createGameStateBuffer = () => {
     return buffer;
 };
 
-export { createGameStateBuffer };
+const getPlayerOnePos = (buffer) => {
+    const view = new DataView(buffer);
+    return view.getUint16(bufferVal.playerOnePos);
+};
+
+const getPlayerTwoPos = (buffer) => {
+    const view = new DataView(buffer);
+    return view.getUint16(bufferVal.playerTwoPos);
+};
+
+const getBallXPos = (buffer) => {
+    const view = new DataView(buffer);
+    return view.getUint16(bufferVal.ballXPos);
+};
+
+const getBallYPos = (buffer) => {
+    const view = new DataView(buffer);
+    return view.getUint16(bufferVal.ballYPos);
+};
+
+const getBallHSpeed = (buffer) => {
+    const view = new DataView(buffer);
+    return view.getFloat32(bufferVal.ballHSpeed);
+};
+
+const getBallVSpeed = (buffer) => {
+    const view = new DataView(buffer);
+    return view.getFloat32(bufferVal.ballVSpeed);
+};
+
+const getPlayerOneScore = (buffer) => {
+    const view = new DataView(buffer);
+    return view.getUint8(bufferVal.playerOneScore);
+};
+
+const getPlayerTwoScore = (buffer) => {
+    const view = new DataView(buffer);
+    return view.getUint8(bufferVal.playerTwoScore);
+};
+
+const setPlayerOnePos = (buffer, newVal) => {
+    const view = new DataView(buffer);
+    view.setUint16(bufferVal.playerOnePos, newVal);
+};
+
+const setPlayerTwoPos = (buffer, newVal) => {
+    const view = new DataView(buffer);
+    view.setUint16(bufferVal.playerTwoPos, newVal);
+};
+
+const setBallXPos = (buffer, newVal) => {
+    const view = new DataView(buffer);
+    view.setUint16(bufferVal.ballXPos, newVal);
+};
+
+const setBallYPos = (buffer, newVal) => {
+    const view = new DataView(buffer);
+    view.setUint16(bufferVal.ballYPos, newVal);
+};
+
+const setBallHSpeed = (buffer, newVal) => {
+    const view = new DataView(buffer);
+    view.setFloat32(bufferVal.ballHSpeed, newVal);
+};
+
+const setBallVSpeed = (buffer, newVal) => {
+    const view = new DataView(buffer);
+    view.setFloat32(bufferVal.ballVSpeed, newVal);
+};
+
+const setPlayerOneScore = (buffer, newVal) => {
+    const view = new DataView(buffer);
+    view.setUint8(bufferVal.playerOneScore, newVal);
+};
+
+const setPlayerTwoScore = (buffer, newVal) => {
+    const view = new DataView(buffer);
+    view.setUint8(bufferVal.playerTwoScore, newVal);
+};
+
+export {
+    createGameStateBuffer,
+    getPlayerOnePos,
+    getPlayerTwoPos,
+    getBallXPos,
+    getBallYPos,
+    getBallHSpeed,
+    getBallVSpeed,
+    getPlayerOneScore,
+    getPlayerTwoScore,
+    setPlayerOnePos,
+    setPlayerTwoPos,
+    setBallXPos,
+    setBallYPos,
+    setBallHSpeed,
+    setBallVSpeed,
+    setPlayerOneScore,
+    setPlayerTwoScore,
+};
