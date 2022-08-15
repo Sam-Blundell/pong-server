9a0fdbb046c000e740efbd3cadd10d39e27cbd94
Sam Blundell <samcalblundell@googlemail.com>
2022-08-15 15:52:14 +0100

    add touch controls for mobile devices

--- client/javascript/inputHandler.js
+++ client/javascript/inputHandler.js
@@ -1,6 +1,9 @@
 export default class InputHandler {
     constructor(game) {
         this.game = game;
+        this.gameScreen = document.getElementById('GameScreen');
+        this.screenWidth = this.gameScreen.clientWidth + 10;
+        this.lastTouch = null;
         this.validKeys = ['ArrowRight', 'ArrowLeft'];
         this.keyVal = {
             ArrowLeft: 0,
@@ -22,6 +25,20 @@ export default class InputHandler {
             if (this.validKeys.includes(event.code)) {
                 this.game.server.sendInput(this.keyVal[event.code], this.actions.up);
             }
+        });
+        this.gameScreen.addEventListener('touchstart', (event) => {
+            event.preventDefault();
+            if (event.touches[0].clientX > this.screenWidth / 2) {
+                this.game.server.sendInput(this.keyVal.ArrowRight, this.actions.down);
+                this.lastTouch = this.keyVal.ArrowRight;
+            } else {
+                this.game.server.sendInput(this.keyVal.ArrowLeft, this.actions.down);
+                this.lastTouch = this.keyVal.ArrowLeft;
+            }
+        });
+        this.gameScreen.addEventListener('touchend', (event) => {
+            event.preventDefault();
+            this.game.server.sendInput(this.lastTouch, this.actions.up);
         });
     }
 }
